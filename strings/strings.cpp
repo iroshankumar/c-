4c051245897e706eb43c2762842407cd94e5aff7
Roshan Kumar <124682247+iroshankumar@users.noreply.github.com>
2024-01-03 23:46:05 +0530

    Add files via upload

---
+++ strings/strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char s[50] = "hello"; // hello line or strings
+    cout << s << endl;
+
+    char s2[] = {'R', 'o', 's', 'h', 'a', 'n'};
+    cout << s2 << endl;
+
+    char s3[] = {65, 66, 67, 68, 69, 70};
+    cout << s3 << endl;
+
+    return 0;
+}
